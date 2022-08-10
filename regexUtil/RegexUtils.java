f275c12b7a364da7f1eafc7af95460311e5803d0
DaHuangGO <85286598+nacey5@users.noreply.github.com>
2022-08-10 12:57:48 +0800

    Create RegexUtils.java

---
+++ regexUtil/RegexUtils.java
@@ -0,0 +1,46 @@
+import cn.hutool.core.util.StrUtil;
+import com.hzh.gatheringproject.generics.RegexPatterns;
+
+public class RegexUtils {
+
+    /**
+     * 验证是否是有效的手机号
+     * @param phone
+     * @return
+     */
+    public static boolean isPhoneInvalid(String phone){
+        return mismatch(phone, RegexPatterns.PHONE_REGEX);
+    }
+
+    /**
+     * 验证是否是有效的邮箱
+     * @param email
+     * @return
+     */
+    public static boolean isEmailInvalid(String email){
+        return mismatch(email,RegexPatterns.EMAIL_REGEX);
+    }
+
+    /**
+     * 验证密码是否有效
+     * @param password
+     * @return
+     */
+    public static boolean isPasswordInvalid(String password){
+        return mismatch(password,RegexPatterns.PASSWORD_REGEX);
+    }
+
+
+    /**
+     * 校验是否不符合正则格式
+     * @param str
+     * @param regex
+     * @return
+     */
+    public static boolean mismatch(String str,String regex){
+        if (StrUtil.isBlank(str)) {
+            return true;
+        }
+        return !str.matches(regex);
+    }
+}
