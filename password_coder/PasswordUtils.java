8155d0d8a54c3c97718a4078bc00515ba97b3c45
DaHuangGO <85286598+nacey5@users.noreply.github.com>
2022-08-12 18:37:54 +0800

    Create PasswordUtils.java

---
+++ password_coder/PasswordUtils.java
@@ -0,0 +1,44 @@
+import java.util.UUID;
+
+/**
+ * @author DAHUANG
+ * @date 2022/8/12
+ * 密文包装类
+ */
+public class PasswordUtils {
+
+
+    /**
+     * 匹配密码
+     *
+     * @param salt    盐
+     * @param rawPass 明文
+     * @param encPass 密文
+     * @return
+     */
+    public static boolean matches(String salt, String rawPass, String encPass) {
+        return new PasswordEncoder(salt).matches(encPass, rawPass);
+    }
+
+    /**
+     * 明文密码加密
+     *
+     * @param rawPass 明文
+     * @param salt
+     * @return
+     */
+    public static String encode(String rawPass, String salt) {
+        return new PasswordEncoder(salt).encode(rawPass);
+    }
+
+    /**
+     * 获取加密盐
+     *
+     * @return
+     */
+    public static String getSalt() {
+        return UUID.randomUUID().toString().replaceAll("-", "").substring(0, 20);
+    }
+
+
+}
