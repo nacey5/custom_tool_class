27a0d30c6b30de08977a1edcf1aaee2ec17db6d7
DaHuangGO <85286598+nacey5@users.noreply.github.com>
2022-08-23 19:26:47 +0800

    Create User.java

---
+++ listener/common/User.java
@@ -0,0 +1,13 @@
+/**
+ * @author DAHUANG
+ * @date 2022/8/22
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class User {
+    private Integer uid;
+    private String username;
+    private String pwd;
+}
