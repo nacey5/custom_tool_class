d7cf74f27116d81093147117721e33415eca0412
DaHuangGO <85286598+nacey5@users.noreply.github.com>
2022-08-23 22:16:41 +0800

    Create MyListener.java

---
+++ listener/application_listener/MyListener.java
@@ -0,0 +1,20 @@
+/**
+ * 监听用户数据:上下文监听器：应用级
+ * @author DAHUANG
+ * @date 2022/8/22
+ */
+@Component
+public class MyListener implements ApplicationListener<ContextRefreshedEvent> {
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        //首先获去application上下文
+        ApplicationContext applicationContext = contextRefreshedEvent.getApplicationContext();
+        //获取对应的service
+        UserService userService = applicationContext.getBean(UserService.class);
+        User user = userService.getUser();
+        //获取application的域对象，将查到的信息放到application域中
+        ServletContext application = applicationContext.getBean(ServletContext.class);
+        application.setAttribute("user",user);
+    }
+}
